6a8043ff1368972a97e70ff890caf7b1cf9e42c5
AdryanOvie2 <adryanovie2@gmail.com>
2023-12-02 21:22:17 +0100

    modified file

--- historias_locas.py
+++ historias_locas.py
@@ -1,15 +1,9 @@
 # historias locas juego
-# Tenemos un parrafo con espacios en blanco para completar por el usuario
-# y luego mostrarle al usuario su historia loca, el texto completado con sus palabras.
-
-# aspectos a tener en cuenta
-# concatenar caracteres
 
 adj = input("Ingresa un Adjetivo: ")
 verb1 = input("Ingresa un Verbo: ")
 verb2 = input("Ingresa otro Verbo: ")
-sustantivo = input("Ingresa un Sustantivo plural: ")
-
+sustantivo = input("Ingresa un Sustantivo (plural): ")
 
 madlib = f"Programar es tan {adj}! Siempre me emociona porque me encanta {verb1} problemas. Aprende a {verb2} en 42Madrid y alcanza tus {sustantivo}!"
 
